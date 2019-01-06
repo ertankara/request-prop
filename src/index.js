0fcb799d07c3da6f6a55d858e5c55ab28bd1a6db
Ertan Kara <kara.ertan9@gmail.com>
2019-01-07 00:45:22 +0300

    Add tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,58 @@
 /**
+ * If user wants to extract object props instead of objects that are
+ * inside the array then this function should operate on the rawData
+ * @param {object} rawObject
+ * @param {array} requestedProps
+ * @return {object}
+ */
+function objectOperator(rawObject, requestedProps, modifications, renameIndication) {
+  const keys = Object.keys(rawObject);
+  const hasModifiers = modifications.length > 0;
+  const newlyConstructedObject = {};
+  console.log('progress 1');
+
+  for (const key of keys) {
+    let propName = key;
+    const indexOfKeyInTheRequestedProps = requestedProps
+      .findIndex(el => el.split(renameIndication)[0] === key);
+
+    // Means that the user has requested this prop
+    if (indexOfKeyInTheRequestedProps !== -1) {
+      console.log('progress 2');
+      const didUserRenamePropName = requestedProps[indexOfKeyInTheRequestedProps]
+        .indexOf(renameIndication) !== -1;
+
+      // If the prop in the requestedProps has a special character `renameIndication` in it
+      // then user wants to rename the prop, so do it
+      if (didUserRenamePropName) {
+        console.log('progress 3')
+        // Get the new name for the prop
+        propName = requestedProps[indexOfKeyInTheRequestedProps].split(renameIndication)[1];
+      }
+
+      // If user wants to perform modifications on the retrieved prop val
+      if (hasModifiers && modifications.includes(key)) {
+        console.log('progress 4');
+        // Get the key's index first and then find the function which
+        // is expected receive
+        const modifier = modifications[modifications.findIndex(el => el === key) + 1];
+        if (!modifier || typeof modifier !== 'function') { // User has to provide a function as a following element
+          throw new Error('Modifiers has to follow the element to operate on');
+        }
+
+        // Operate on data and store it
+        newlyConstructedObject[propName] = modifier(rawObject[key]);
+      } else { // If user provided no modifier simply bind data
+        console.log('progress 5');
+        newlyConstructedObject[propName] = rawObject[key];
+      }
+    }
+  }
+
+  return newlyConstructedObject;
+}
+
+/**
  * Returns a new array from the given array of objects
  * with objects but only requested props
  * @param {object} rawData
@@ -17,69 +71,29 @@ function requestProps(rawData, requestedProps, modifications = [], renameIndicat
   if (
     rawData === undefined ||
     !Array.isArray(requestedProps)
-  ) throw new Error('Raw data and required props have to be specified');
+  ) {
+    throw new Error('Raw data and required props have to be specified');
+  }
 
   if (!Array.isArray(rawData)) {
+    console.log('Progress 6')
     return objectOperator(rawData, requestedProps, modifications, renameIndication);
   }
 
   const extractedData = [];
 
   for (const data of rawData) {
-    const newlyConstructedObject = objectOperator(data, requestedProps, modifications, renameIndication);
+    const newlyConstructedObject = objectOperator(
+      data,
+      requestedProps,
+      modifications,
+      renameIndication
+    );
 
-    extractedData.push(newlyConstructedObject)
+    extractedData.push(newlyConstructedObject);
   }
 
   return extractedData;
 }
 
-
-/**
- * If user wants to extract object props instead of objects that are
- * inside the array then this function should operate on the rawData
- * @param {object} rawObject
- * @param {array} requestedProps
- * @return {object}
- */
-function objectOperator(rawObject, requestedProps, modifications, renameIndication) {
-  const keys = Object.keys(rawObject);
-  const hasModifiers = modifications.length > 0;
-  const newlyConstructedObject = {};
-
-  for (const key of keys) {
-    let propName = key;
-    const indexOfKeyInTheRequestedProps = requestedProps.findIndex(el => el.split(renameIndication)[0] === key);
-
-    // Means that the user has requested this prop
-    if (indexOfKeyInTheRequestedProps !== -1) {
-      const didUserRenamePropName = requestedProps[indexOfKeyInTheRequestedProps]
-        .indexOf(renameIndication) !== -1;
-
-      // If the prop in the requestedProps has a special character `renameIndication` in it
-      // then user wants to rename the prop, so do it
-      if (didUserRenamePropName) {
-        // Get the new name for the prop
-        propName = requestedProps[indexOfKeyInTheRequestedProps].split(renameIndication)[1];
-      }
-
-      // If user wants to perform modifications on the retrieved prop val
-      if (hasModifiers && modifications.includes(key)) {
-        // Get the key's index first and then find the function which
-        // is expected receive
-        const modifier = modifications[modifications.findIndex(el => el === key) + 1];
-        if (!modifier || typeof modifier !== 'function') // User has to provide a function as a following element
-          throw new Error('Modifiers has to follow the element to operate on');
-
-        // Operate on data and store it
-        newlyConstructedObject[propName] = modifier(rawObject[key]);
-      } else { // If user provided no modifier simply bind data
-        newlyConstructedObject[propName] = rawObject[key];
-      }
-    }
-  }
-
-  return newlyConstructedObject;
-}
-
-// export default requestProps;
+export default requestProps;
--- test/index.js
+++ test/index.js
@@ -1,14 +1,163 @@
 import { assert } from 'chai';
-import defaultAwesomeFunction, { awesomeFunction } from '../src';
+import RequestProps from '../src';
 
-describe('Awesome test.', () => {
-  it('should test default awesome function', () => {
-    const expectedVal = 'I am the Default Awesome Function, fellow comrade! - Dinesh'
-    assert(defaultAwesomeFunction('Dinesh') === expectedVal, 'Default not awesome :(');
+const exampleArray = [
+  { id: 'f34ddva-gfghhvv-7775ffggcv', name: 'Jane', lastname: 'Doe', age: 26, favLanguage: 'JavaScript' },
+  { id: 'f34ddva-gfghhvv-554fffdcca', name: 'Marry', lastname: 'Poppins', age: 19, favLanguage: 'Java' },
+  { id: 'f34ddva-gfghhvv-6688ggggcc', name: 'John', lastname: 'Johnson', age: 35, favLanguage: 'Python' }
+];
+
+describe('Main function behaviour', () => {
+  it('should return the requested props', () => {
+    let expectedVal = true;
+
+    const complexObjectWhichHasMorePropsThanINeed = {
+      id: 'f34ddva-gfghhvv-7775ffggcv',
+      name: 'Jane',
+      lastname: 'Doe',
+      age: 26,
+      favLanguage: 'JavaScript'
+    };
+
+    const retrievedVal = RequestProps(
+      complexObjectWhichHasMorePropsThanINeed,
+      ['name', 'lastname']
+    )
+
+    for (const key of Object.keys(retrievedVal)) {
+      const expectedKeys = ['name', 'lastname'];
+      if (!expectedKeys.includes(key)) {
+        expectedVal = false;
+      }
+    }
+
+
+    assert(expectedVal, 'Failed to return requested props');
   });
 
-  it('should test awesome function', () => {
-    const expectedVal = 'I am just an Awesome Function'
-    assert(awesomeFunction() === expectedVal, 'Named awesome :(');
+
+  it('shouldn\'t alter the value of the props while returning the object', () => {
+    let expectedVal = true;
+
+    const complexObjectWhichHasMorePropsThanINeed = {
+      id: 'f34ddva-gfghhvv-7775ffggcv',
+      name: 'Jane',
+      lastname: 'Doe',
+      age: 26,
+      favLanguage: 'JavaScript'
+    };
+
+    const retrievedVal = RequestProps(
+      complexObjectWhichHasMorePropsThanINeed,
+      ['name', 'lastname']
+    )
+
+    for (const key of Object.keys(retrievedVal)) {
+      const expectedKeys = ['name', 'lastname'];
+      if (
+        !expectedKeys.includes(key) &&
+        retrievedVal[key] !== complexObjectWhichHasMorePropsThanINeed[key]
+      ) {
+        expectedVal = false;
+      }
+    }
+
+    assert(expectedVal, 'The keys are retrieved but values don\'t match');
   });
+
+
+  it('should be able to extract objects out of array with requested props', () => {
+    let expectedVal = true;
+
+    const retrievedVal = RequestProps(exampleArray, ['name']);
+
+    for (const obj of retrievedVal) {
+      if (!obj.name) expectedVal = false;
+      else if (obj.name.favLanguage) expectedVal = false;
+    }
+
+    assert(expectedVal, 'The objects returned from arrays contains expected props')
+  });
+
+  it('should be able to extract objects out of array with requested props without altering the value', () => {
+    const retrievedVal = RequestProps(exampleArray, ['name', 'id']);
+
+    let expectedVal = false;
+
+    for (const val of retrievedVal) {
+      for (const obj of exampleArray) {
+        if (val.id === obj.id) {
+          expectedVal = val.name === obj.name;
+        }
+      }
+    }
+
+    assert(expectedVal, 'The objects returned from arrays contains expected props and values match')
+  });
+});
+
+
+describe('Altering prop name', () => {
+  it('should be able to alter prop name', () => {
+    let expectedVal = true;
+    const retrievedVal = RequestProps(exampleArray, ['name:userName', 'id:userId']);
+
+    for (const val of retrievedVal) {
+      if (!val.hasOwnProperty('userName')) expectedVal = false;
+      else if (!val.hasOwnProperty('userId')) expectedVal = false;
+    }
+
+    assert(expectedVal, 'Failed to alter the name of the retrieved prop');
+  });
+});
+
+
+describe('Applying modifications', () => {
+  it('should apply modificaitions successfully', () => {
+    let expectedVal = true;
+
+    const retrievedVal = RequestProps(
+      exampleArray,
+      ['age', 'id'],
+      ['age', function tenYearsLater(age) { return age + 10 }]
+    );
+
+    for (const val of retrievedVal) {
+      for (const obj of exampleArray) {
+        if (val.id === obj.id) {
+          if (!(val.age === obj.age + 10)) {
+            expectedVal = false;
+          }
+        }
+      }
+    }
+
+
+    assert(expectedVal, 'Failed to apply modifications on retrieved props');
+  });
+});
+
+describe('Throws error', () => {
+  it('should throw error when one of the first two arguments are missing', () => {
+    let expectedVal = false;
+    try {
+      RequestProps();
+    } catch(e) {
+      expectedVal = true;
+    }
+
+    assert(expectedVal, 'Failed to throw Error when arguments are missing');
+  });
+
+  it('should throw error when operand is given but no operator is given', () => {
+    let expectedVal = false;
+
+    try {
+      RequestProps(exampleArray, ['name:Name'], ['name']);
+    } catch(e) {
+      expectedVal = true;
+    }
+
+    assert(expectedVal, 'Failed to throw an error when modifier is not given');
+  })
 });
